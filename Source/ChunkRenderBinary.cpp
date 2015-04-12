a5b373f55ba86db4d0fd180ada799b769c4e7c62
HertzDevil <nicetas.c@gmail.com>
2015-04-12 14:22:39 +0800

    final touch-up

--- Source/ChunkRenderBinary.cpp
+++ Source/ChunkRenderBinary.cpp
@@ -2,8 +2,6 @@
 ** FamiTracker - NES/Famicom sound tracker
 ** Copyright (C) 2005-2014  Jonathan Liss
 **
-** 0CC-FamiTracker is (C) 2014-2015 HertzDevil
-**
 ** This program is free software; you can redistribute it and/or modify
 ** it under the terms of the GNU General Public License as published by
 ** the Free Software Foundation; either version 2 of the License, or
--- Source/InstrumentEditorVRC7.cpp
+++ Source/InstrumentEditorVRC7.cpp
@@ -2,6 +2,8 @@
 ** FamiTracker - NES/Famicom sound tracker
 ** Copyright (C) 2005-2014  Jonathan Liss
 **
+** 0CC-FamiTracker is (C) 2014-2015 HertzDevil
+**
 ** This program is free software; you can redistribute it and/or modify
 ** it under the terms of the GNU General Public License as published by
 ** the Free Software Foundation; either version 2 of the License, or
@@ -470,7 +472,7 @@ void CInstrumentEditorVRC7::PasteSettings(LPCTSTR pString)
 	std::istream_iterator<std::string> end;
 
 	for (int i = 0; (i < 8) && (begin != end); ++i) {
-		int value = CSequenceInstrumentEditPanel::ReadStringValue(*begin++, false);
+		int value = CSequenceInstrumentEditPanel::ReadStringValue(*begin++, false);		// // //
 		value = std::min<int>(std::max<int>(value, 0x00), 0xFF);
 		m_pInstrument->SetCustomReg(i, value);
 	}
