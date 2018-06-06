ca318f81dc73d78575b2a8428f3d60d0c604c309
nyanpasu64 <nyanpasu64@tuta.io>
2018-06-06 06:16:02 -0700

    Add CLion IDE (CMake build) (#86)
    * clion lol
    
    * Update CMakeLists.txt
    
    * Partially fix Windows build (CMakeLists.txt)
    
    * Remove unneeded include directories
    
    * Edit .gitignore to exclude cmake-build-*/
    
    * Don't include res/0CC-FamiTracker.manifest twice (fix linking errors)
    
    * """enable parallel builds""" (useless, just use jom instead of nmake)
    
    * Revert """"enable parallel builds""" (useless, just use jom instead of nmake)"
    
    This reverts commit f4984547997a81c92b7e8c7dbb0873a047df1160.
    
    * CMakeLists.txt: Remove ${PROJECT} variable.
    
    project() sets ${PROJECT_NAME}, which I use instead.
    
    * Add mfc.cmake file. Fix MFC configuration for release builds.
    
    Adapted from https://github.com/Kitware/CMake/blob/master/Tests/MFC/CMakeLists.txt.in
    The CMake source tree is distributed under terms of the BSD 3-Clause license.
    
    * Move cmake includes into separate folder
    
    * Configure CLion to use tabs for indentation

---
+++ cmake/mfc.cmake
@@ -0,0 +1,49 @@
+# taken from https://github.com/Kitware/CMake/blob/master/Tests/MFC/CMakeLists.txt.in
+# The CMake source tree is distributed under terms of the BSD 3-Clause license.
+
+
+macro(replace_flags var these those)
+    if("${${var}}" MATCHES "${these}")
+        string(REGEX REPLACE "${these}" "${those}" ${var} "${${var}}")
+        #message(STATUS "info: ${var} changed to '${${var}}'")
+    endif()
+    message(STATUS "info: ${var}='${${var}}'")
+endmacro()
+
+macro(msvc_link_to_static_crt)
+    if(MSVC)
+        set(has_correct_flag 0)
+        foreach(lang C CXX)
+            foreach(suffix "" _DEBUG _MINSIZEREL _RELEASE _RELWITHDEBINFO)
+                replace_flags("CMAKE_${lang}_FLAGS${suffix}" "/MD" "/MT")
+                if(CMAKE_${lang}_FLAGS${suffix} MATCHES "/MT")
+                    set(has_correct_flag 1)
+                endif()
+            endforeach()
+        endforeach()
+        if(NOT has_correct_flag)
+            message(FATAL_ERROR "no CMAKE_*_FLAGS var contains /MT")
+        endif()
+    endif()
+endmacro()
+
+
+set(CMAKE_MFC_FLAG "@CMAKE_MFC_FLAG_VALUE@")
+
+FIND_PACKAGE(MFC)
+IF (NOT MFC_FOUND)
+    MESSAGE(FATAL_ERROR "MFC Could not be found during the MFC test")
+ENDIF()
+
+if("${CMAKE_MFC_FLAG}" STREQUAL "1")
+    msvc_link_to_static_crt()
+else()
+    # VS generators add this automatically based on the CMAKE_MFC_FLAG value,
+    # but generators matching "Make" require:
+    add_definitions(-D_AFXDLL)
+endif()
+
+if("${CMAKE_MFC_FLAG}" STREQUAL "2")
+    set(CMAKE_INSTALL_MFC_LIBRARIES ON)
+    include(InstallRequiredSystemLibraries)
+endif()
