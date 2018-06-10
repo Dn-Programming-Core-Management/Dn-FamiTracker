20638523daea52e6ec54e3b318304ab2d04abc08
nyanpasu64 <nyanpasu64@tuta.io>
2018-06-10 03:25:12 -0700

    Add NSF driver build script (build.cmd)

---
+++ Source/drivers/build.cmd
@@ -0,0 +1,11 @@
+set drivers=%cd%
+cd asm\build || goto :error
+
+lua build_engine.lua || goto :error
+copy drivers\* %drivers%\ || goto :error
+rmdir /s /q drivers || goto :error
+goto :EOF
+
+:error
+echo Failed with error #%errorlevel%.
+exit /b %errorlevel%
