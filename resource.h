326cfd4d799f8e936b0087452e27f16febebceba
Persune <54422576+Gumball2415@users.noreply.github.com>
2021-07-28 20:02:02 +0800

    Redesign configuration UI

--- resource.h
+++ resource.h
@@ -799,7 +799,7 @@
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        377
 #define _APS_NEXT_COMMAND_VALUE         33215
-#define _APS_NEXT_CONTROL_VALUE         1497
+#define _APS_NEXT_CONTROL_VALUE         1500
 #define _APS_NEXT_SYMED_VALUE           179
 #endif
 #endif
