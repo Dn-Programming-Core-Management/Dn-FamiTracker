dd173710b1c98df17ab60cb13c6abc3dbf865094
Persune <54422576+Gumball2415@users.noreply.github.com>
2021-09-14 13:52:32 +0800

    Include debug builds in build script

--- release.bat
+++ release.bat
@@ -1,20 +1,30 @@
 mkdir distribute
-mkdir distribute\x64
-mkdir distribute\x86
+mkdir distribute\x64\Release
+mkdir distribute\x64\Debug
+mkdir distribute\x86\Release
+mkdir distribute\x86\Debug
 
-copy x64\Release\Dn-FamiTracker.exe distribute\x64\Dn-FamiTracker.exe
-copy x64\Release\Dn-FamiTracker.chm distribute\x64\Dn-FamiTracker.chm
-copy x64\Release\Dn-FamiTracker.pdb distribute\x64\Dn-FamiTracker.pdb
-copy x64\Release\vc141.pdb distribute\x64\vc141.pdb
-copy changelog.txt distribute\x64\changelog.txt
-copy 0CC-readme.txt distribute\x64\0CC-readme.txt
+copy x64\Release\Dn-FamiTracker.exe	distribute\x64\Release\Dn-FamiTracker.exe
+copy x64\Release\Dn-FamiTracker.chm	distribute\x64\Release\Dn-FamiTracker.chm
+copy x64\Release\Dn-FamiTracker.pdb	distribute\x64\Release\Dn-FamiTracker.pdb
+copy x64\Release\vc142.pdb 			distribute\x64\Release\vc142.pdb
 
-copy Win32\Release\Dn-FamiTracker.exe distribute\x86\Dn-FamiTracker.exe
-copy Win32\Release\Dn-FamiTracker.chm distribute\x86\Dn-FamiTracker.chm
-copy Win32\Release\Dn-FamiTracker.pdb distribute\x86\Dn-FamiTracker.pdb
-copy Win32\Release\vc141.pdb distribute\x86\vc141.pdb
-copy changelog.txt distribute\x86\changelog.txt
-copy 0CC-readme.txt distribute\x86\0CC-readme.txt
+copy x64\Debug\Dn-FamiTracker.exe	distribute\x64\Debug\Dn-FamiTracker.exe
+copy x64\Debug\Dn-FamiTracker.chm	distribute\x64\Debug\Dn-FamiTracker.chm
+copy x64\Debug\Dn-FamiTracker.pdb	distribute\x64\Debug\Dn-FamiTracker.pdb
+copy x64\Debug\vc142.pdb			distribute\x64\Debug\vc142.pdb
+copy changelog.txt					distribute\x64\changelog.txt
+
+copy Win32\Release\Dn-FamiTracker.exe	distribute\x86\Release\Dn-FamiTracker.exe
+copy Win32\Release\Dn-FamiTracker.chm	distribute\x86\Release\Dn-FamiTracker.chm
+copy Win32\Release\Dn-FamiTracker.pdb	distribute\x86\Release\Dn-FamiTracker.pdb
+copy Win32\Release\vc142.pdb			distribute\x86\Release\vc142.pdb
+
+copy Win32\Debug\Dn-FamiTracker.exe		distribute\x86\Debug\Dn-FamiTracker.exe
+copy Win32\Debug\Dn-FamiTracker.chm		distribute\x86\Debug\Dn-FamiTracker.chm
+copy Win32\Debug\Dn-FamiTracker.pdb		distribute\x86\Debug\Dn-FamiTracker.pdb
+copy Win32\Debug\vc142.pdb				distribute\x86\Debug\vc142.pdb
+copy changelog.txt						distribute\x86\changelog.txt
 
 
 @echo.
