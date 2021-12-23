08f34e1cc863939b3620ce15da6c16705538a2d5
Persune <54422576+Gumball2415@users.noreply.github.com>
2021-12-23 19:42:33 +0800

    Update source code copyright declaration and GPL 2.0+ notice (#116)

--- Source/HistoryFileDlg.h
+++ Source/HistoryFileDlg.h
@@ -1,3 +1,27 @@
+/*
+** FamiTracker - NES/Famicom sound tracker
+** Copyright (C) 2005-2015 Jonathan Liss
+**
+** 0CC-FamiTracker is (C) 2014-2018 HertzDevil
+**
+** Dn-FamiTracker is (C) 2020-2021 D.P.C.M.
+**
+** This program is free software; you can redistribute it and/or modify
+** it under the terms of the GNU General Public License as published by
+** the Free Software Foundation; either version 2 of the License, or
+** (at your option) any later version.
+**
+** This program is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+** Library General Public License for more details. To obtain a
+** copy of the GNU Library General Public License, write to the Free
+** Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+**
+** Any permitted reproduction of these routines, in whole or in part,
+** must bear this legend.
+*/
+
 #pragma once
 
 #include "stdafx.h"
