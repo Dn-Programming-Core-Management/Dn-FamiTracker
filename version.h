6b521234dd9e38f767c0390007f5598e655b48c8
Persune <gumball2415@gmail.com>
2023-05-10 17:13:33 +0800

    Increment version to 0.5.0.1

--- version.h
+++ version.h
@@ -31,13 +31,13 @@
 
 
 // Define this for beta builds
-#define WIP
+// #define WIP
 
 
 #define VERSION_API  0
 #define VERSION_MAJ  5
 #define VERSION_MIN  0
-#define VERSION_REV  0
+#define VERSION_REV  1
 
 #define VERSION VERSION_API,VERSION_MAJ,VERSION_MIN,VERSION_REV
 
