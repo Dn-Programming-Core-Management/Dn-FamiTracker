43b1d528fd0a31eaee6fe0fd83ba9ba9d1c514ea
Persune <gumball2415@gmail.com>
2023-11-29 01:46:27 +0800

    Fix access violation in MRU submenu list update
    Fixes #243.

--- Source/FamiTracker.cpp
+++ Source/FamiTracker.cpp
@@ -455,11 +455,11 @@ void CFamiTrackerApp::OnRecentFilesClear()		// // //
 
 void CFamiTrackerApp::OnUpdateRecentFiles(CCmdUI *pCmdUI)		// // //
 {
-	// https://www.codeguru.com/cpp/controls/menu/miscellaneous/article.php/c167
+	// https://web.archive.org/web/20190906190854/https://www.codeguru.com/cpp/controls/menu/miscellaneous/article.php/c167/MRU-list-in-a-submenu-the-MFC-bug-and-how-to-correct-it.htm
 	// updating a submenu?
 	if (pCmdUI->m_pSubMenu != NULL) return;
 
-	m_pRecentFileList->UpdateMenu(pCmdUI);
+	CWinApp::OnUpdateRecentFileMenu(pCmdUI);
 }
 
 void CFamiTrackerApp::ShutDownSynth()
