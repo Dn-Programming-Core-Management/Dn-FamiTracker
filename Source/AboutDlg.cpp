39bff44b1b9cddfa44b630d90bc47ea421d3ad7b
Persune <gumball2415@gmail.com>
2025-08-08 18:12:18 +0800

    Rename contributor to Sun Rays
    - This resolves #362.

--- Source/AboutDlg.cpp
+++ Source/AboutDlg.cpp
@@ -168,7 +168,7 @@ BOOL CAboutDlg::OnInitDialog()
 		_T("- Sunsoft 5B information in manual by forple\r\n")		// // !!
 		_T("- Additional manual information by Persune\r\n")		// // !!
 		_T("- Toolbar icons are made by ilkke\r\n")
-		_T("- Dn-FT icon design by Pale Moon\r\n")		// // !!
+		_T("- Dn-FT icon design by Sun Rays\r\n")		// // !!
 		_T("- DPCM import resampler by Jarhmander\r\n")
 		_T("- DPCM sample bit order reverser mod by Persune\r\n")		// // !!
 		_T("- Module text import/export by rainwarrior"));		// // //
