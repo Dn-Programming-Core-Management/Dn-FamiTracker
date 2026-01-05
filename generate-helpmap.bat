6c8829e5ac5f8fad9dfca82ccb790b03eabc59e4
Persune <gumball2415@gmail.com>
2026-01-05 21:23:08 +0800

    Refactor custom build commands
    - Put custom build commands in their own script, so that other build
      systems can use them.
    - Deduplicate custom build commands in projects

--- generate-helpmap.bat
+++ generate-helpmap.bat
@@ -1,6 +1,3 @@
-rem call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvarsall.bat" x86_amd64
-@echo off
-powershell -Command "(Get-Content Dn-help\hlp\changelog_shell.htm).Replace('<!-- INSERT-CHANGELOG-HERE -->', (pandoc CHANGELOG.md -f gfm -t html5)) | Set-Content -encoding UTF8 Dn-help\hlp\changelog.htm"
 echo // Generated Help Map file.  Used by Dn-FamiTracker.hhp. > "Dn-help\hlp\HTMLDefines.h"
 echo. > "Dn-help\hlp\HTMLDefines.h"
 echo // Commands (ID_* and IDM_*) >> "Dn-help\hlp\HTMLDefines.h"
@@ -17,12 +14,3 @@ makehm /h IDD_,HIDD_,0x20000 "resource.h" >> "Dn-help\hlp\HTMLDefines.h"
 echo. >> "Dn-help\hlp\HTMLDefines.h"
 echo // Frame Controls (IDW_*) >> "Dn-help\hlp\HTMLDefines.h"
 makehm /h /a "afxhh.h" IDW_,HIDW_,0x50000 "resource.h" >> "Dn-help\hlp\HTMLDefines.h"
-start /wait hhc "Dn-help\hlp\Dn-FamiTracker.hhp"
-if not exist "Dn-help\hlp\Dn-FamiTracker.chm" goto :HelpError
-move "Dn-help\hlp\Dn-FamiTracker.chm" "Dn-FamiTracker.chm"
-goto :HelpDone
-:HelpError
-echo Dn-help\hlp\Dn-FamiTracker.hhp(1) : error:Problem encountered creating help file
-echo.
-:HelpDone
-echo.
